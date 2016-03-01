b5776145dbe8477d3bedb832f15a5bf17d1ce2be
Jamcobs <jamcobs21@gmail.com>
2016-03-01 12:57:19 -0800

    constants.h

---
+++ solution--James-Jacobs/constants.h
@@ -0,0 +1,19 @@
+ #ifndef CONSTANTS_H
+ #define CONSTANTS_H
+ // ----------------------------------------------------------------------------
+
+ namespace {
+   const unsigned int TERM_SIZE_X = 80;
+
+    const unsigned int TERM_SIZE_Y = 25;
+
+   const double RATIO_FILLED = 0.40;
+ 
+   const double RATIO_ALIKE_HAPPY = 1 / 2.0;
+  
+   const double RATIO_DIFFERENT_HAPPY = 1 / 5.0;
+ 
+ }
+ 
+ // ----------------------------------------------------------------------------
+ #endif
